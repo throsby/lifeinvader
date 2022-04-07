91f7fbee08497192fb34f1d1ae90b5c6b1ab82c7
Throsby Wells <throsbywells@gmail.com>
2022-04-07 17:33:39 -0400

    added basic invader

---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import Timeline from "./Timeline"
+import Sidebar from "./Sidebar"
+
+const Homepage = () => {
+    return (
+        <div>
+            <h2>Homepage</h2>
+            <Timeline />
+            <Sidebar />
+        </div>
+    )
+}
+
+export default Homepage
