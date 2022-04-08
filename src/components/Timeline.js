49d6d137a93f5821735225288cbab3206d056bb9
Throsby Wells <throsbywells@gmail.com>
2022-04-08 13:25:23 -0400

    posts on timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -4,19 +4,31 @@ import { useEffect, useState } from "react"
 const Timeline = () => {
     const [posts, setPosts] = useState([])
     
-    useEffect(async () => {
-    console.log("Rendering timeline...")
-    let req = await fetch("https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json")
-    let res = await req.json();
-    setPosts(res)
+    useEffect(()=>{
+        // console.log("rendering timeline")
+        // IIFE: Immediately invoked function expression
+        (async () => {
+            let req = await fetch("https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json")
+            let res = await req.json();
+            setPosts(res)
+        })()
     },[])
-
+    
     return (
         <div>
             <h1>Timeline</h1>
-            {/* {
-                posts.map()
-            } */}
+            {
+                posts.map((element) => {
+                    return(
+                        <div>
+                            <p>Post by {element.username}</p>
+                            <p>{element.content}</p>
+                        </div>
+                    )
+                }
+
+                )
+            }
         </div>
     )
 }
